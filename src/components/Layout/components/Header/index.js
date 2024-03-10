6c4353bece0fa175b0be7eedfb83e244eacbbe4b
cocorino259 <dominhhoangvu@gmail.com>
2024-03-10 08:52:58 +0700

    My commit

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,5 +1,20 @@
+import classNames from 'classnames/bind';
+import styles from './Header.module.scss';
+
+const cx = classNames.bind(styles);
+
 function Header() {
-    return <h2>Header</h2>;
+    return (
+        <header className={cx('wrapper')}>
+            <div className={cx('inner')}>
+                <div className={cx('logo')}>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="118" height="42" fill="currentColor" alt="TikTok">
+                        <use xlink:href="#logo-dark-c4561408"></use>
+                    </svg>
+                </div>
+            </div>
+        </header>
+    );
 }
 
 export default Header;
